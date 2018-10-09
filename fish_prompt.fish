7413f8b2004b98233915115eb9a1895155faa605
Dušan Simić <dusan.simic1810@gmail.com>
2018-10-09 07:36:18 +0200

    Added config

---
+++ fish_prompt.fish
@@ -0,0 +1,83 @@
+# name: clearance
+# ---------------
+# Based on idan. Display the following bits on the left:
+# - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
+# - Current directory name
+# - Git branch and dirty state (if inside a git repo)
+# - SSH user and host if using it
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _git_is_dirty
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
+
+function _is_ssh
+	echo "$SSH_CONNECTION"
+end
+
+function fish_prompt
+  set -l last_status $status
+
+  set -l cyan (set_color cyan)
+  set -l yellow (set_color yellow)
+  set -l red (set_color red)
+  set -l blue (set_color blue)
+  set -l green (set_color green)
+  set -l normal (set_color normal)
+	set -l special_gray (set_color 93A1A1)
+
+  set -l cwd $blue(pwd | sed "s:^$HOME:~:")
+
+  # Output the prompt, left to right
+
+  # Add a newline before new prompts
+  echo -e ''
+
+  # Display [venvname] if in a virtualenv
+  if set -q VIRTUAL_ENV
+      echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
+  end
+
+  # Print pwd or full path
+  echo -n -s $cwd $normal
+
+  # Show git branch and status
+  if [ (_git_branch_name) ]
+    set -l git_branch (_git_branch_name)
+
+    if [ (_git_is_dirty) ]
+      set git_info '(' $yellow $git_branch "±" $normal ')'
+    else
+      set git_info '(' $green $git_branch $normal ')'
+    end
+    echo -n -s ' · ' $git_info $normal
+  end
+
+	# Show user and host if ssh
+	if [ (_is_ssh) ]
+		set -l user (whoami)
+		set -l host (hostname -s)
+
+		if [ "$user" = "root" ]
+			set user "$user"
+		else
+			set user "$special_gray$user"
+		end
+
+		set user_and_host "$user$special_gray@$host"
+
+		echo -n -s ' · ' $user_and_host $normal
+	end
+
+  set -l prompt_color $red
+  if test $last_status = 0
+    set prompt_color $normal
+  end
+
+  # Terminate with a nice prompt char
+  echo -e ''
+  echo -e -n -s $prompt_color '⟩ ' $normal
+end
